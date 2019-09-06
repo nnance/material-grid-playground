370ab311211c5caa0a481753820d1218145583f3
Nick Nance <nance.nick@gmail.com>
2019-09-05 20:39:32 -0700

    minor improvement on dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
+import { makeStyles, createStyles, useTheme } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      flexGrow: 1,
-    },
-    paper: {
-      padding: theme.spacing(2),
-      textAlign: 'center',
-      color: theme.palette.text.secondary,
-    },
-  }),
-);
+const useStyles = makeStyles(theme => createStyles({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
 
 export default function FullWidthGrid() {
-  const classes = useStyles();
+  const classes = useStyles(useTheme());
 
   return (
     <div className={classes.root}>
@@ -45,10 +43,15 @@ export default function FullWidthGrid() {
             <Paper className={classes.paper}>xs=6 sm=3</Paper>
           </Grid>
         </Grid>
-        <Grid container item xs={2} spacing={3} >
+        <Grid container item xs={2}>
           <Grid item xs={12}>
             <Paper className={classes.paper}>xs=12</Paper>
           </Grid>
+          <Grid item xs={12}>
+            <Paper className={classes.paper}>xs=12</Paper>
+          </Grid>
+        </Grid>
+        <Grid container item xs={12}>
           <Grid item xs={12}>
             <Paper className={classes.paper}>xs=12</Paper>
           </Grid>
